af431fdb625ec976de7787e4a39f15f8ddb36856
Christian Rodriguez <rodrigch18@up.edu>
2017-12-03 20:59:47 -0800

    cropped images

--- trainer.py
+++ trainer.py
@@ -3,8 +3,25 @@ import cv2
 import numpy as np
 from PIL import Image
 
+cascPath = "lbpcascade_frontalface.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 path = 'dataSet'
+
+def preprocess():
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    for im in imagePaths:
+        img = cv2.imread(im)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=7,
+            minSize=(30, 30)
+        )
+        for (x, y, w, h) in faces:
+            sub_face = img[y:y+h, x:x+w]
+            cv2.imwrite(im, sub_face)
 
 def getImagesWithID(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
@@ -18,7 +35,8 @@ def getImagesWithID(path):
         ids.append(imageID)
     return ids, facesList
 
-
+for i in range(20):
+    preprocess()
 ids,faces = getImagesWithID(path)
 recognizer.train(faces,np.array(ids))
 recognizer.write('recognizer/trainingData.yml')
